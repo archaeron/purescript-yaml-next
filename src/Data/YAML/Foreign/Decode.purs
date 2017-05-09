849484bf3f1934df3802d8b84f28bbc45152058d
Dominick Gendill <dgendill@gmail.com>
2017-05-09 14:42:47 -0600

    Allow YAML to be converted into Json

--- src/Data/YAML/Foreign/Decode.purs
+++ src/Data/YAML/Foreign/Decode.purs
@@ -1,18 +1,24 @@
-module Data.YAML.Foreign.Decode (parseYAML, readYAMLGeneric) where
+module Data.YAML.Foreign.Decode (parseYAML, readYAMLGeneric, parseYAMLToJson) where
 
 import Data.Foreign (F, Foreign, ForeignError(..), fail)
-import Data.Foreign.Generic.Class (class GenericDecode)
 import Data.Foreign.Generic (genericDecode)
+import Data.Foreign.Generic.Class (class GenericDecode)
 import Data.Foreign.Generic.Types (Options)
 import Data.Function.Uncurried (Fn3, runFn3)
 import Data.Generic.Rep (class Generic)
-import Prelude ((>=>), (<<<), pure)
+import Prelude ((>=>), (<<<), pure, (>>=))
+import Unsafe.Coerce (unsafeCoerce)
+import Data.Argonaut.Core (Json)
 
 foreign import parseYAMLImpl :: forall r. Fn3 (String -> r) (Foreign -> r) String r
 
 -- | Attempt to parse a YAML string, returning the result as foreign data.
 parseYAML :: String -> F Foreign
 parseYAML yaml = runFn3 parseYAMLImpl (fail <<< JSONError) pure yaml
+
+-- | Attempt to parse a YAML string, returning the result as Json
+parseYAMLToJson :: String -> F Json
+parseYAMLToJson yaml = parseYAML yaml >>= pure <<< unsafeCoerce
 
 -- | Automatically generate a YAML parser for your data from a generic instance.
 readYAMLGeneric :: forall a rep. (Generic a rep) => (GenericDecode rep) => Options -> String -> F a
